5251af39d65739f4a4dfd6d1047bd3c73ed16fbf
genghailong <ghl.1987@foxmail.com>
2023-11-08 18:00:23 +0800

    config

--- src/vue.js
+++ src/vue.js
@@ -1,34 +1,35 @@
-import { toArray } from "./util";
+import {toArray} from "./util";
 import state from "./state";
+import lifecycle from "./lifecycle";
 
 class Vue {
-  constructor(options) {
-    this.init(options);
-  }
-
-  /**
-   * 初始化入口
-   */
-
-  init(options) {
-    this._directives = [];
-    this._watchers = [];
-
-    const el = document.querySelector(options.el);
-
-    options._containerAttrs = toArray(el.attributes);
-
-    this.$options = options;
-
-    // 合并options，这样可以使用 this.xxx() 调用 options 的method xxx
-    for (let k in options.methods) {
-      this[k] = options.methods[k];
+    constructor(options) {
+        this.init(options);
     }
 
-    this._initState();  // state 里面定义的
+    /**
+     * 初始化入口
+     */
 
-    this._compile(el, options); // lifecycle 里面定义的
-  }
+    init(options) {
+        this._directives = [];
+        this._watchers = [];
+
+        const el = document.querySelector(options.el);
+
+        options._containerAttrs = toArray(el.attributes);
+
+        this.$options = options;
+
+        // 合并options，这样可以使用 this.xxx() 调用 options 的method xxx
+        for (let k in options.methods) {
+            this[k] = options.methods[k];
+        }
+
+        this._initState();  // state 里面定义的
+
+        this._compile(el, options); // lifecycle 里面定义的
+    }
 }
 
 // mixins lifecycle
